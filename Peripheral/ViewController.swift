223c324b216cdec9016246617e528146b511f6f4
-- C4 -- <info@c4ios.com>
2015-07-08 23:22:06 -0700

    Add peripheral code

--- Peripheral/ViewController.swift
+++ Peripheral/ViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CBPeripheralManagerDelegate {
+    var peripheralManager : CBPeripheralManager?
+
+    let connection_service_uuid = CBUUID(string: "39BB9101-9800-4C6D-B032-CAC5ABEA1B76")
+    let transfer_service_uuid = CBUUID(string: "4268FA37-EADC-4C47-AFF8-15B4569BDE05")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        peripheralManager = CBPeripheralManager(delegate: self, queue: dispatch_get_main_queue())
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,43 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //MARK:-
+    //MARK: Peripheral Manager
+    //MARK: Monitoring
+    func peripheralManagerDidUpdateState(peripheral: CBPeripheralManager!) {
+        println("peripheralManagerDidUpdateState")
 
+        if peripheral.state != .PoweredOn {
+            return
+        }
+
+        let service = CBMutableService(type: transfer_service_uuid, primary: true)
+        peripheralManager?.addService(service)
+        peripheralManager?.startAdvertising([CBAdvertisementDataServiceUUIDsKey : [connection_service_uuid]])
+    }
+
+    func peripheralManager(peripheral: CBPeripheralManager!, willRestoreState dict: [NSObject : AnyObject]!) {
+        println("will restore state")
+    }
+
+    func peripheralManager(peripheral: CBPeripheralManager!, didAddService service: CBService!, error: NSError!) {
+        println("didAddService")
+    }
+
+    func peripheralManager(peripheral: CBPeripheralManager!, central: CBCentral!, didSubscribeToCharacteristic characteristic: CBCharacteristic!) {
+        println("didSubscribeToCharacteristic")
+    }
+
+    func peripheralManager(peripheral: CBPeripheralManager!, central: CBCentral!, didUnsubscribeFromCharacteristic characteristic: CBCharacteristic!) {
+        println("didUnsubscribeToCharacteristic")
+    }
+
+    func peripheralManager(peripheral: CBPeripheralManager!, didReceiveReadRequest request: CBATTRequest!) {
+        println("didReceiveReadRequest")
+    }
+
+    func peripheralManager(peripheral: CBPeripheralManager!, didReceiveWriteRequests requests: [AnyObject]!) {
+        println("didReceiveWriteRequests")
+    }
 }
 
